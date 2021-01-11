8c63577633af90ea158611480acd361e495609d4
Gonzalo Faraone <gfaraone@litebox.ai>
2021-01-11 19:42:29 -0300

    Remove loader

--- src/components/DelayedWrapper.tsx
+++ src/components/DelayedWrapper.tsx
@@ -1,46 +1,52 @@
-import React, { FC, useState, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { FC, useState, useEffect } from "react";
+import styled from "styled-components";
 
-const Wrapper = styled.div``
+const Wrapper = styled.div``;
 
 const Main = styled.div<{ show: boolean }>`
-    opacity: ${props => props.show ? 1 : 0};
-    transition: opacity .2s linear;
-    min-height: 100vh;
-`
+  opacity: ${props => (props.show ? 1 : 0)};
+  transition: opacity 0.2s linear;
+  min-height: 100vh;
+`;
 
 const Loader = styled.div<{ noHeader: boolean }>`
-    display: flex;
-    align-items: center;
-    height: calc(100vh - ${props => props.noHeader ? '130px' : '260px'});
-    width: 50px;
-    margin: 0 auto;
-    img {
-        max-width: 100%;
-    }
-`
+  display: flex;
+  align-items: center;
+  height: calc(100vh - ${props => (props.noHeader ? "130px" : "260px")});
+  width: 50px;
+  margin: 0 auto;
+  img {
+    max-width: 100%;
+  }
+`;
 
 type Props = {
-    noHeader?: boolean,
-    time?: number
-}
+  noHeader?: boolean;
+  time?: number;
+};
 
-const DelayedWrapper: FC<Props> = ({ children, noHeader = false, time = 1000 }) => {
-    const [show, setShow] = useState(false);
+const DelayedWrapper: FC<Props> = ({
+  children,
+  noHeader = false,
+  time = 0
+}) => {
+  const [show, setShow] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => setShow(true), time)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    setTimeout(() => setShow(true), time);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return <Wrapper>
-        {!show && <Loader noHeader={noHeader}>
-            <img src="/images/loader.svg" alt="loader" />
-        </Loader>}
-        <Main show={show}>
-            {children}
-        </Main>
+  return (
+    <Wrapper>
+      {!show && (
+        <Loader noHeader={noHeader}>
+          <img src="/images/loader.svg" alt="loader" />
+        </Loader>
+      )}
+      <Main show={show}>{children}</Main>
     </Wrapper>
-}
+  );
+};
 
 export default DelayedWrapper;
