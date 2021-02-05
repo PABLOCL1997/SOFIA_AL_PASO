52639c71a0dacfeddda0a0fe44a0b6c776c9fbba
Cristian Sagula <csagula@litebox.ai>
2021-02-05 12:03:47 -0300

    change branch

--- src/components/Images/HeaderLogo.tsx
+++ src/components/Images/HeaderLogo.tsx
@@ -10,18 +10,39 @@ const Image = styled.img`
   }
 `;
 
+const Container = styled.div`
+  img {
+    @media (max-width: ${LG}) {
+      height: auto !important;
+    }
+  }
+`;
+
 type Props = {
   withSlogan?: boolean;
 };
 
 const HeaderLogo: FC<Props> = () => {
   return (
-    <Image
+    /*     <Image
       width="83px"
       height="50px"
       src="/images/sofia-logo.svg"
       alt="Sofía"
     />
+ */
+    <Container>
+      <picture>
+        <source srcSet={"/images/sofia-logo.webp 2x"} type="image/webp" />
+        <source srcSet={"/images/sofia-logo.png 2x"} type="image/jpeg" />
+        <img
+          src={"/images/sofia-logo.png"}
+          width="83px"
+          height="50px"
+          alt={"Sofía"}
+        />
+      </picture>
+    </Container>
   );
 };
 export default HeaderLogo;
