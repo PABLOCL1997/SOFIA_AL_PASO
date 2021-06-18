50838cd67a0451ac7b13f5afdc1b66b1da03d3a4
danibarriadev <daniel.barria@ibisdev.tech>
2021-06-18 08:11:43 -0300

    new page incentivos

---
+++ src/pages/incentivos.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+  margin: 45px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+`
+
+const Incentivos = () => {
+    return (
+        <Wrapper>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdnZZBPZYi8VkLDzoQJV2I3RQdzLco4p1KcooxqF57-qz-Jfg/viewform?embedded=true" width="800" height="1400" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
+        </Wrapper>
+    )
+}
+
+export default Incentivos
