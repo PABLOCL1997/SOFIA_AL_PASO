cc53b125f4794c1d243790357842fb7f3db00d36
José Daniel Barría Reyes <dbarria@litebox.ai>
2021-09-08 20:00:23 +0000

    now redirects to home when no-token or expired

--- src/pages/myaccount.tsx
+++ src/pages/myaccount.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 import { useLocation } from "react-router-dom";
 import { MY_ACCOUNT_TITLE } from '../meta';
 import { BREAKPOINT } from '../utils/constants';
+import { token } from '../utils/store';
+import { useQuery } from '@apollo/react-hooks';
+import { CHECK_TOKEN } from '../graphql/user/queries';
+import useUser from '../hooks/useUser';
 
 const Loader = React.lazy(() => import(/* webpackChunkName: "Loader" */'../components/Loader'));
 const Sidebar = React.lazy(() => import(/* webpackChunkName: "Sidebar" */'../components/MyAccount/Sidebar'));
@@ -64,6 +68,9 @@ const Container = styled.div`
 
 type Props = {}
 const MyAccount: FC<Props> = () => {
+    const history = useHistory();
+    const { data } = useQuery(CHECK_TOKEN, { fetchPolicy: "network-only" });
+    const { logout, toggleLoginModal } = useUser();
     const { pathname } = useLocation();
     const [showDetails, setShowDetails] = useState(pathname.indexOf('ordenes') < 0);
 
@@ -71,6 +78,28 @@ const MyAccount: FC<Props> = () => {
         document.title = MY_ACCOUNT_TITLE;
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    useEffect(() => {
+        // when user is on micuenta page with token but expired or
+        // when user is on mi-cuenta without token
+      if (
+        (token.get() !== "null" &&
+        data &&
+        data.checkToken &&
+        !data.checkToken.status) ||
+        token.get() == "null"
+      ) {
+        logout();
+        token.delete();
+  
+        setTimeout(() => {
+          history.push("/?login=true");
+        }, 0);
+      }
+  
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data]);
 
     useEffect(() => {
         setShowDetails(pathname.indexOf('ordenes') < 0);
