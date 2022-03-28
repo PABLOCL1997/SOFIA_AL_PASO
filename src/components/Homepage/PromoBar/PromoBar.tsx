f90a60bfa441067067a933d343fa4046aff19e07
federico avelin <favelin@litebox.ai>
2022-03-28 11:47:01 -0300

    restore first code

--- src/components/Homepage/PromoBar/PromoBar.tsx
+++ src/components/Homepage/PromoBar/PromoBar.tsx
@@ -38,7 +38,7 @@ const PromoBar: FC = () => {
         <SC.Buy.TitleDesktop>{"Comprar →"}</SC.Buy.TitleDesktop>
         <SC.Buy.TitleMobile>{"Comprar con Sofia Express →"}</SC.Buy.TitleMobile>
       </SC.Buy.Button>
-      <SC.Close onClick={hideBar as any}>
+      <SC.Close onClick={() => hideBar()}>
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M16 2L2 16" stroke="#fff" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square" />
           <path d="M16 16L2 2" stroke="#fff" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="square" />
