5f5c345dc78368b360f55009a68377c5a3d1e02f
federico avelin <favelin@litebox.ai>
2022-03-31 14:28:56 -0300

    add hook for get window width/height with debounce

---
+++ src/hooks/useWindowDimesions.ts
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+function debounce(func: any) {
+  let timer: NodeJS.Timeout;
+  return function () {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(func, 100);
+  };
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    const debounceResize = debounce(handleResize);
+
+    window.addEventListener("resize", debounceResize);
+    return () => window.removeEventListener("resize", debounceResize);
+  }, []);
+
+  return windowDimensions;
+}
