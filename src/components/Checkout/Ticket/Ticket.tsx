e55390f48ef06b9d750f9e9a49989a535fc16b4f
eperniola <eperniola@litebox.ai>
2022-04-07 18:25:13 -0300

    Removido limite compra

--- src/components/Checkout/Ticket/Ticket.tsx
+++ src/components/Checkout/Ticket/Ticket.tsx
@@ -36,7 +36,6 @@ const Ticket: FC<Props> = ({ order, updateOrder, processing, userData, userDetai
 
   const [showCoupon, setShowCoupon] = useState(false);
   const [coupon, setCoupon] = useState("");
-  const [enableBuy, setEnableBuy] = useState(true);
 
   const [getCartItems] = useLazyQuery(GET_CART_ITEMS, {
     fetchPolicy: "network-only",
@@ -97,11 +96,6 @@ const Ticket: FC<Props> = ({ order, updateOrder, processing, userData, userDetai
   useEffect(() => {
     getCartItems();
   }, [localUserData]);
-
-  useEffect(() => {
-    if (isExpress) setEnableBuy(Number(totalAmount.replace(",", ".")) > minimumPrice);
-    else setEnableBuy(true);
-  }, [totalAmount, minimumPrice]);
 
   return (
     <Suspense fallback={<Loader />}>
@@ -180,7 +174,7 @@ const Ticket: FC<Props> = ({ order, updateOrder, processing, userData, userDetai
 
         {step === Steps.Review ? (
           <SC.CtaWrapper>
-            {!processing && <Cta active={ready && enableBuy && Number(totalAmount.replace(",", ".")) > 0} text={t("checkout.ticket.send")} action={order} filled />}
+            {!processing && <Cta active={ready && Number(totalAmount.replace(",", ".")) > 0} text={t("checkout.ticket.send")} action={order} filled />}
             {processing && (
               <SC.LoaderWrapper>
                 <img src="/images/loader.svg" width="50px" height="50px" alt="loader" />
@@ -189,7 +183,6 @@ const Ticket: FC<Props> = ({ order, updateOrder, processing, userData, userDetai
           </SC.CtaWrapper>
         ) : null}
         {shippingPrice > 0 ? <SC.ErrorText margin={false}>A partir de Bs. {minimumPrice} el envio es gratis.</SC.ErrorText> : null}
-        {!enableBuy && <SC.ErrorText margin={false}>{t("checkout.ticket.minimum_buy_limit", { sum: minimumPrice })}</SC.ErrorText>}
       </SC.Container>
     </Suspense>
   );
