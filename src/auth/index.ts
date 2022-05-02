c075e4ab7f767c038a7c9be5bd413f7929ff6407
federico avelin <favelin@litebox.ai>
2022-05-02 10:28:38 -0300

    use graphql mutations to singup and recover password in auth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,38 +1,14 @@
-import axios from "axios";
-import { print } from "graphql";
+import { useMutation } from "react-apollo";
 import { RECOVER, SIGN_UP } from "../graphql/user/mutations";
 
-interface Props {
-  email: string;
-  firstname: string;
-  lastname: string;
-  password: string;
-}
+const useAuth = () => {
+  const [signUp] = useMutation(SIGN_UP);
+  const [recoverPassword] = useMutation(RECOVER);  
 
-export const signUp = async ({ email, firstname, lastname, password }: Props) => {
-  const response = await axios.post(process.env.REACT_APP_GRAPHQL as string, {
-    query: print(SIGN_UP),
-    variables: {
-      email,
-      firstname,
-      lastname,
-      password,
-      network: false
-    },
-  });
+  return {
+    signUp,
+    recoverPassword
+  }
+};
 
-  return response.data;    
-} 
-
-export const recoverPassword = async(email: string) => {
-  const response = await axios.post(process.env.REACT_APP_GRAPHQL as string, {
-    query: print(RECOVER),
-    variables: {
-      email,
-      url: process.env.REACT_APP_SITE_URL + "/password-reset",
-      bompras: false
-    }
-  });
-
-  return response.data;
-}
+export default useAuth;
