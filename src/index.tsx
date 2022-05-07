6333ee856eb9dbdcc527038423651659991ecb16
favelin <favelin@litebox.ai>
2022-05-07 16:45:50 -0300

    remove redux from dependencies and delete all config for redux

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,11 @@ import { ApolloProvider } from "react-apollo";
 import "./i18n";
 import createClient from "./apollo";
 import "react-datepicker/dist/react-datepicker.css";
-import { store } from "./state/store";
-import { Provider } from 'react-redux';
 
 createClient().then((client) => {
   ReactDOM.render(
     <ApolloProvider client={client}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ApolloProvider>,
     document.getElementById("root")
   );
