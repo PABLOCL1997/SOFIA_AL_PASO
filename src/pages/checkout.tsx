3cc0ec6d70bb73c781c62153243e234db1a0fcbc
federico avelin <favelin@litebox.ai>
2022-05-11 12:21:54 -0300

    remove confirm address for checkout

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -32,7 +32,6 @@ const Review = React.lazy(() => import(/* webpackChunkName: "Review" */ "../comp
 const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ "../components/Checkout/Steps/Cart"));
 const DeliveryDate = React.lazy(() => import(/* webpackChunkName: "DeliveryDate" */ "../components/Checkout/Steps/DeliveryDate"));
 const Ticket = React.lazy(() => import(/* webpackChunkName: "Ticket" */ "../components/Checkout/Ticket"));
-const ConfirmAddress = React.lazy(() => import(/* webpackChunkName: "ConfirmAddress" */ "../components/Checkout/ConfirmAddress"));
 
 const Checkout = () => {
   const { t } = useTranslation();
@@ -471,13 +470,7 @@ const Checkout = () => {
   };
 
   return (
-    <Suspense fallback={<Loader />}>
-      <ConfirmAddress
-        address={orderData.shipping.street ? orderData.shipping.street.replace(/\|/g, " ") : ""}
-        visible={confirmModalVisible}
-        confirm={saveOrder}
-        cancel={() => setConfirmModalVisible(false)}
-      />
+    <Suspense fallback={<Loader />}>      
       <SC.Wrapper>
         <div className="main-container">
           {!showTodotixPayment && !result.length && (
