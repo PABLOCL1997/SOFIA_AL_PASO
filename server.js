bba5dd0a9c715120f69c7c9501b33edd3b501112
eperniola <eperniola@litebox.ai>
2022-07-26 17:22:08 -0300

    Removido campos sin uso

--- server.js
+++ server.js
@@ -382,8 +382,7 @@ app.use(expressCspHeader({
       'style-src': [SELF, 'https://sofia.com.bo/'],
       'img-src': ['data:', 'https://sofia.com.bo/'],
       'worker-src': [NONE],
-      'block-all-mixed-content': false,
-      'frame-ancestors': 'https://sofia.com.bo/'
+      'block-all-mixed-content': false
   }
 }));
 
